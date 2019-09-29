28bab4eabc34c06fbfa50440cbb66dea84235f22
unknown <aadjamoglian@gmail.com>
2019-09-28 19:37:45 -0700

    Second commit enter the commit message for your changes. Lines starting

---
+++ PyBank/main.py
@@ -0,0 +1,69 @@
+import os
+import csv
+import statistics
+os.getcwd()
+
+filepath = os.path.join("Desktop", "Python Challenges", "Pybank", "budget_data_copy.csv")
+
+with open(filepath, 'r', newline="") as csvfile:
+    csvcontent = csv.reader(csvfile, delimiter=",")
+
+    header = next(csvcontent)
+    tot_months = sum(1 for line in csvcontent)
+
+total = 0
+
+
+with open(filepath, 'r', newline="") as csvfile:
+    csvcontent = list(csv.reader(csvfile, delimiter=","))
+    
+    maxvalrow = csvcontent[26]
+    maxvaldate = maxvalrow[0]
+
+    minvalrow = csvcontent[45]
+    minvaldate = minvalrow[0]
+    
+
+with open(filepath, 'r', newline="") as csvfile:
+    csvcontent = csv.reader(csvfile, delimiter=",")
+
+    profloss = []
+
+    header = next(csvcontent)
+    for i in csvcontent:
+        profloss.append(i[1])
+
+    new_profloss = []
+
+    for j in profloss:
+        new_profloss.append(int(j))
+    profloss = new_profloss
+
+    for k in new_profloss:
+        total = total + k
+
+    avg_change = round((total/tot_months),2)
+
+print("Financial Analysis")
+print("------------------------------")
+print(f"Total Months: {tot_months}")
+print(f"Total: ${total}")
+print(f"Average Change: ${avg_change}")
+print(f"Greatest Increase in Profits: {maxvaldate} (${max(new_profloss)})")
+print(f"Greatest Decrease in Profits: {minvaldate} (${min(new_profloss)})")
+
+minpos = new_profloss.index(min(new_profloss))
+
+maxpos = new_profloss.index(max(new_profloss))
+
+f= open("PyBankAnalysis.txt","w+")
+
+f.write("Financial Analysis")
+f.write("\n------------------------------")
+f.write(f"\nTotal Months: {tot_months}")
+f.write(f"\nTotal: ${total}")
+f.write(f"\nAverage Change: ${avg_change}")
+f.write(f"\nGreatest Increase in Profits: {maxvaldate} (${max(new_profloss)})")
+f.write(f"\nGreatest Decrease in Profits: {minvaldate} (${min(new_profloss)})")
+
+f.close()
---
+++ PyPoll/main.py
@@ -0,0 +1,59 @@
+import os
+import csv
+import operator
+
+filepath = os.path.join("Desktop", "Python Challenges", "PyPoll", "election_data.csv")
+
+
+vote_dict = {}
+vote_count = 0
+
+with open(filepath, 'r', newline="") as csvfile:
+    pypoll_csv = csv.reader(csvfile, delimiter=",")
+
+    next(pypoll_csv)
+    for row in pypoll_csv:
+        if row[2] not in vote_dict:
+            vote_dict.update({row[2]:1})
+            vote_count += 1
+
+        else:
+            vote_dict[row[2]] += 1
+            vote_count += 1
+
+locals().update(vote_dict)
+
+khan_percent = round((Khan/vote_count*100),3)
+correy_percent = round((Correy/vote_count*100),3)
+li_percent = round((Li/vote_count*100),3)
+otooley_percent = round(((vote_dict["O'Tooley"])/vote_count*100),3)
+
+OTooley = vote_dict["O'Tooley"]
+
+winner = max(vote_dict.items(), key=operator.itemgetter(1))[0]
+
+print("Election Results")
+print("-------------------------")
+print(f"Total Votes: {vote_count}")
+print("-------------------------")
+print(f"Khan: {khan_percent}% ({Khan})")
+print(f"Correy: {correy_percent}% ({Correy})")
+print(f"Correy: {li_percent}% ({Li})")
+print(f"Correy: {otooley_percent}% ({OTooley})")
+print("-------------------------")
+print(f"Winner: {winner}")
+
+f = open("PyPollAnalysis.txt", "w+")
+
+f.write("Election Results")
+f.write("\n-------------------------")
+f.write(f"\nTotal Votes: {vote_count}")
+f.write("\n-------------------------")
+f.write(f"\nKhan: {khan_percent}% ({Khan})")
+f.write(f"\nCorrey: {correy_percent}% ({Correy})")
+f.write(f"\nCorrey: {li_percent}% ({Li})")
+f.write(f"\nCorrey: {otooley_percent}% ({OTooley})")
+f.write("\n-------------------------")
+f.write(f"\nWinner: {winner}")
+
+f.close()
